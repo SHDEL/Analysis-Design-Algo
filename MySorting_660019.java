0b8a7cb049ac38405e97d782d63117509416caf0
Kongphop Worawutkasem <del2547.pv@gmail.com>
2025-01-09 16:22:29 +0700

    first commit

---
+++ MySorting_660019.java
@@ -0,0 +1,84 @@
+import java.util.Arrays;
+
+public class MySorting_660019 {
+    static int[] nums = { 5, 1, 1, 2, 0, 0 };
+    public static void main(String[] args) {
+        MySorting_660019 sort = new MySorting_660019();
+        sort.mergeSort();
+        System.out.println(Arrays.toString(nums));
+    }
+    public void mergeSort() {
+        mergeSort(nums, 0, nums.length - 1);
+    }
+    private void mergeSort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    private void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public void quickSort() {
+        quickSort(nums, 0, nums.length - 1);
+    }
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = lomutoParition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    private int lomutoParition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+}
